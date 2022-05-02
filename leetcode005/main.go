51dc426ac4b4941e174fa0b87d22e89f7e7ba1e6
AntonyIS <antony@ariyafinergy.com>
2022-05-02 10:39:45 +0300

    adding Go solution for Sum of two

--- leetcode005/main.go
+++ leetcode005/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	a := 2
+	b := 10
+
+	sum := getSum(a, b)
+
+	fmt.Println(sum)
+}
+
+func getSum(a, b int) int {
+	XOR := a ^ b
+	AND := a & b
+
+	if AND == 0 {
+		return XOR
+	}
+	return getSum(XOR, AND<<1)
 }
